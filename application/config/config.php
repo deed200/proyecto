5e44224b4f288bbaaf4d5d5decb75fb0aa5a3e6e
Proweb2013 <Proweb2013@10.0.2.15>
2014-01-31 16:44:10 +0100

    Modificado la calve de encriptacion para las sessiones

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost';
+$config['base_url']	= 'http://localhost/proyecto_test/';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'gt44r7JPDGYsLmjTzoUaEn9M0ADDSM3P';
 
 /*
 |--------------------------------------------------------------------------
