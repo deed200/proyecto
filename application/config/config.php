6031304c34a5ebfbf88630673acf139c404a5e5a
Proweb2013 <Proweb2013@10.0.2.15>
2014-01-31 17:19:03 +0100

    Poner base_url = ''

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/proyecto_test/';
+$config['base_url']	= '';
 
 /*
 |--------------------------------------------------------------------------
