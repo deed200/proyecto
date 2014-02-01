e6dbacd3a4bc1fa59c027dc2ca30930d0a9259bd
Proweb2013 <Proweb2013@10.0.2.15>
2014-02-01 17:11:47 +0100

    Comfiguraciones global y plantilla

--- application/views/libros/index.php
+++ application/views/libros/index.php
@@ -1,7 +1,146 @@
-<?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+<section class="layout-1">
+    <section class="find">
+        <input value="Buscar..."/>
+        <img src="assets/styles/images/buscar.png" alt="Buscar" title="Buscar" width="20" height="20" />
+    </section>
+    <nav class="menu-lateral" >
+    <ul >
+        <li><a href="#" title="CategorÃ­a Hogar"><img src="assets/styles/images/hogar.png" alt="CategorÃ­a hogar" title="CategorÃ­a hogar" width="15" height="14"/>Hogar</a></li>
+        <li>
+            <ul>
+                <li><a href="#" title="CategorÃ­a Hogar - Juegos">Juegos</a></li>
+                <li><a href="#" title="CategorÃ­a Hogar - Manualidades">Manualidades</a></li>
+                <li><a href="#" title="CategorÃ­a Hogar - Estilos de vida">Estilos de vida</a></li>
+            </ul>
+        </li>
+        <li><a href="#" title="CategorÃ­a Fantasia"><img src="assets/styles/images/fantasia.png" alt="CategorÃ­a fantasia" title="CategorÃ­a fantasia" width="15" height="18"/>Fantasia</a></li>
+        <li>
+            <ul>
+                <li><a href="#" title="CategorÃ­a Fantasia - Fantasia">Fantasia</a></li>
+                <li><a href="#" title="CategorÃ­a Fantasia - Terror">Terror</a></li>
+                <li><a href="#" title="CategorÃ­a Fantasia - Ciencia y ficciÃ³n">Ciencia y ficciÃ³n</a></li>
+            </ul>
+        </li>
+        <li><a href="#" title="CategorÃ­a Literatura"><img src="assets/styles/images/literatura.png" alt="CategorÃ­a literatura" title="CategorÃ­a literatura" width="15" height="22"/>Literatura</a></li>
+        <li><a href="#" title="CategorÃ­a Arte"><img src="assets/styles/images/arte.png" alt="CategorÃ­a arte" title="CategorÃ­a arte" width="15" height="15"/>Arte</a></li>
+        <li>
+            <ul>
+                <li><a href="#" title="CategorÃ­a Arte - Arte">Arte</a></li>
+                <li><a href="#" title="CategorÃ­a Arte - Cine">Cine</a></li>
+                <li><a href="#" title="CategorÃ­a Arte -ForografÃ­a">Fotograf&iacute;a</a></li>
+            </ul>
+        </li>
+        <li><a href="#" title="CategorÃ­a InformÃ¡tica"><img src="assets/styles/images/informatica.png" alt="CategorÃ­a informÃ¡tica" title="CategorÃ­a informÃ¡tica" width="15" height="15"/>Inform&aacute;tica</a></li>
+        <li><a href="#" title="CategorÃ­a Ciencia"><img src="assets/styles/images/ciencia.png" alt="CategorÃ­a ciencia" title="CategorÃ­a ciencia" width="15" height="17"/>Ciencia</a></li>
+    </ul>
+    </nav>
+    <img class="banner" src="assets/styles/images/banner-lateral.jpg" alt="Anuncio universidad" title="Anuncio Universidad"/>
+</section>
+
+<section class="layout-2-a" >
+    <article class="slider">
+        <img src="assets/styles/images/banner.png" alt="Anuncio1" title="Anuncio1">
+        <p>Deja que la libertad reine.<br>El sol nunca se pone sobre tan glorioso logro humano.</p>
+        <button>DETALLES<img src="assets/styles/images/detalles.png" alt="detalle" title="detalle"></button>
+    </article> 
+    
+    <article class="display-inline-block">
+        <h2>Novedades</h2>
+        <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/mandela.png" alt="libro" title="libro" /><br>
+                <p class="titulo">El largo camino hacia la libertad</p>
+                <p class="autor">Nelson Mandela</p>
+                <p class="editorial">AGUILAR</p>
+                <p class="precio">29,90&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/kick-ass.png" alt="logo" /><br>
+                <p class="titulo">Kick-Ass 2</p>
+                <p class="autor">Mark Millar - John Romita Jr.</p>
+                <p class="editorial">PANINI COMICS</p>
+                <p class="precio">15,75&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/pilares.png" alt="logo" /><br>
+                <p class="titulo">Los pilares de la tierra</p>
+                <p class="autor">Ken Follet</p>
+                <p class="editorial">PLAZA JAN&Eacute;S</p>
+                <p class="precio">35,50&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/pepa-pig.png" alt="logo" /><br>
+                <p class="titulo">Peppa Ilumina Letras y n&uacute;meros</p>
+                <p class="autor">Anaya</p>
+                <p class="editorial">ANAYA</p>
+                <p class="precio">9,50&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/marco-polo.png" alt="logo" /><br>
+                <p class="titulo">Marco Polo no fue solo</p>
+                <p class="autor">Pilar Lozano</p>
+                <p class="editorial">Alta Mar</p>
+                <p class="precio">46,90&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+    </article>
+
+     <article class="display-inline-block">
+         <h2>Lo m&aacute;s le&iacute;do</h2>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/pilares.png" alt="logo" /><br>
+                <p class="titulo">Los pilares de la tierra</p>
+                <p class="autor">Ken Follet</p>
+                <p class="editorial">PLAZA JAN&Eacute;S</p>
+                <p class="precio">35,50&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+        <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/mandela.png" alt="logo" /><br>
+                <p class="titulo">El largo camino hacia la libertad</p>
+                <p class="autor">Nelson Mandela</p>
+                <p class="editorial">AGUILAR</p>
+                <p class="precio">29,90&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/marco-polo.png" alt="logo" /><br>
+                <p class="titulo">Marco Polo no fue solo</p>
+                <p class="autor">Pilar Lozano - Alejandro ROdriguez </p>
+                <p class="editorial">Alta Mar</p>
+                <p class="precio">46,90&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+        <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/kick-ass.png" alt="logo" /><br>
+                <p class="titulo">Kick-Ass 2</p>
+                <p class="autor">Mark Millar - John Romita Jr.</p>
+                <p class="editorial">PANINI COMICS</p>
+                <p class="precio">15,75&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+         <a href="ficha.php">
+            <section class="libro">
+                <img height="211" width="130" src="uploads/images/libros/pepa-pig.png" alt="logo" /><br>
+                <p class="titulo">Peppa Ilumina Letras y n&uacute;meros</p>
+                <p class="autor">Anaya</p>
+                <p class="editorial">ANAYA</p>
+                <p class="precio">9,50&euro; <img src="assets/styles/images/carrito.png" alt="Detalles libro" title="Detalles libro"></p>
+            </section>
+        </a>
+    </article>
+
+</section>
